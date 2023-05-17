6ceaf1cb7739b379c0666723b289b4782efb5db4
Alysson Melo <alyssoncm@gmail.com>
2023-05-17 16:16:48 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ContainersElixirPhoenixSample.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(), # Removed :gettext
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
